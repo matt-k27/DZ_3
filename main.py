bd4a537e74da7116a74482ce52005f14a3dfa878
Matt <matt@MacBook-Pro-Matt.local>
2024-02-23 14:21:58 +0100

    input number + reverse a number

---
+++ main.py
@@ -0,0 +1,18 @@
+print('DZ3')
+
+inputtedNumber = int(input('Enter your number pls : '))
+
+numerator, denominator = divmod(inputtedNumber, 10)
+outputNumber = str(denominator)
+numerator, denominator = divmod(numerator, 10)
+outputNumber += str(denominator)
+numerator, denominator = divmod(numerator, 10)
+outputNumber += str(denominator)
+numerator, denominator = divmod(numerator, 10)
+outputNumber += str(denominator)
+numerator, denominator = divmod(numerator, 10)
+outputNumber += str(denominator)
+
+print(outputNumber)
+
+print('Thank you for using')
