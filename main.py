84207541f522f8e0376d82bbf8f341fbe7382e31
Matt <matt@MacBook-Pro-Matt.local>
2024-02-26 12:01:07 +0100

    new realisation

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ print('DZ3')
 
 inputtedNumber = int(input('Enter your number pls : '))
 
-numerator, denominator = divmod(inputtedNumber, 10)
-outputNumber = str(denominator)
-numerator, denominator = divmod(numerator, 10)
-outputNumber += str(denominator)
-numerator, denominator = divmod(numerator, 10)
-outputNumber += str(denominator)
-numerator, denominator = divmod(numerator, 10)
-outputNumber += str(denominator)
-numerator, denominator = divmod(numerator, 10)
-outputNumber += str(denominator)
+numerator, denominator = divmod(inputtedNumber, 10000)
+outputNumber = numerator
+
+numerator, denominator = divmod(denominator, 1000)
+outputNumber += numerator*10
+
+numerator, denominator = divmod(denominator, 100)
+outputNumber += numerator*100
+
+numerator, denominator = divmod(denominator, 10)
+outputNumber += numerator*1000
+
+numerator, denominator = divmod(denominator, 1)
+outputNumber += numerator*10000
 
 print(outputNumber)
 
-############################################
-
-print(str(inputtedNumber)[::-1])
 
 print('Thank you for using')
